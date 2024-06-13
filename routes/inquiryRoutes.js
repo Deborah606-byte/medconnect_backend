3020c2e17b6a1523a07363ff6db2bd35b2a4baa8
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-06-13 02:59:38 +0000

    restructure project folders

--- routes/inquiryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { submitInquiry } = require("../controllers/inquiryController");
-
-const router = express.Router();
-
-router.post("/submit-inquiry", submitInquiry);
-
-module.exports = router;
