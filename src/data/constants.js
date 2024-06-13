3bde388ed5dd4e4e9696e40c7e0246780f37eafb
Deborah606-byte <paintsildeborah248@gmail.com>
2024-06-13 02:59:38 +0000

    restructure project folders

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,11 @@
+const FE_URL_DEV = "http://localhost:3000";
+const FE_URL_LIVE = "";
+
+module.exports.CORS_OPTIONS = {
+  origin: [FE_URL_DEV, FE_URL_LIVE],
+  credentials: true,
+};
+
 module.exports.URLS = {
   root: "/api",
   user: { root: "/users", all: "/", one: "/:id" },
--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const { dbConnect } = require("./config/db");
 const { config } = require("./config/env");
 const { api } = require("./routes/index");
-const { URLS } = require("./data/constants");
+const { URLS, CORS_OPTIONS } = require("./data/constants");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(CORS_OPTIONS));
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan("common"));
 app.use(URLS.root, api);
 
 dbConnect().then((status) => {
