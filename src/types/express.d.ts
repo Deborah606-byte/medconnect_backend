2fd745079a531d54ddd884f2835af7ccdb2ee4cb
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-06-22 22:56:02 +0000

    add middleware for auth

---
+++ src/types/express.d.ts
@@ -0,0 +1,8 @@
+import type { Request } from "express";
+import type { TokenData } from "./chps-compound";
+
+declare module "express-serve-static-core" {
+  interface Request {
+    auth?: TokenData;
+  }
+}
