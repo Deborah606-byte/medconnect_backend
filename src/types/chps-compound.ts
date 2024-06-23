507cd2459b7f2dc6a84b10677fe60f00bef865e2
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-06-23 01:12:43 +0000

    add reset password endpoint

--- src/types/chps-compound.ts
+++ src/types/chps-compound.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 import { roleSchema, staffSchema } from "../db/schemas/staff";
-import { userSchema, loginDataSchema } from "../db/schemas/user";
+import {
+  userSchema,
+  loginDataSchema,
+  resetPasswordDataSchema,
+} from "../db/schemas/user";
 import { chpsCompoundSchema } from "../db/schemas/chps-compound";
 
 type ChpsData = z.infer<typeof chpsCompoundSchema>;
@@ -8,5 +12,6 @@ export type LoginData = z.infer<typeof loginDataSchema>;
 export type UserData = z.infer<typeof userSchema>;
 export type RoleData = z.infer<typeof roleSchema>;
 export type StaffData = z.infer<typeof staffSchema>;
+export type ResetPasswordData = z.infer<typeof resetPasswordDataSchema>;
 export type ChpsCompundData = ChpsData & Omit<UserData, "chpsCompoundId">;
 export type TokenData = { user: string; staff: string; role: string };
