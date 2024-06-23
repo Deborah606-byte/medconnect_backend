6f38711adf336de6f560d28c42c71cfa23dd8f0e
Deborah606-byte <paintsildeborah248@gmail.com>
2024-06-23 22:03:03 +0000

    add super admin endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { URLS } from "../config/constants";
 import { auth } from "./auth";
+import { admin } from "./admin";
 import { chps } from "./chps-compund";
 import { inquiry } from "./inquiry";
 import { patient } from "./patient";
@@ -10,6 +11,7 @@ import { authenticate } from "../middleware/auth-requests";
 const router = express.Router();
 
 router.use(URLS.auth.root, auth);
+router.use(URLS.admin.root, authenticate, admin);
 router.use(URLS.chps.root, authenticate, chps);
 router.use(URLS.patient.root, authenticate, patient);
 router.use(URLS.inquiry.root, authenticate, inquiry);
