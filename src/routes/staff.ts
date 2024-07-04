85640a1713bb2ec727cfde66813b1b74c843e6b8
Deborah606-byte <paintsildeborah248@gmail.com>
2024-07-04 15:25:25 +0000

    setup staff root route

--- src/routes/staff.ts
+++ src/routes/staff.ts
@@ -1,4 +1,13 @@
 import express from "express";
 import { URLS } from "../config/constants";
+import { authorizeUser } from "../middleware/auth-requests";
 
 const router = express.Router();
+
+router
+  .route(URLS.staff.all)
+  .all(authorizeUser)
+  .get(fetchStaff)
+  .post(createStaff);
+
+export const staff = router;
