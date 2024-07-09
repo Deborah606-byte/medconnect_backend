2d28c3169de47dd6e590108ca9696a5a9fdb5165
Deborah606-byte <paintsildeborah248@gmail.com>
2024-07-09 03:25:17 +0000

    refactor id generator service

--- src/services/id.ts
+++ src/services/id.ts
@@ -7,59 +7,78 @@ interface CompoundResource extends Document {
   chpsCompoundId: ObjectId;
 }
 
-export class IDGenerator<T extends CompoundResource> {
-  private prefix: string = "MDC";
-  private modelName: string = "";
-  private modelInstance: T;
-  private currentId: string | undefined = undefined;
-  private error: string = `Failed to generate id for ${this.modelName.toLowerCase()}`;
+abstract class BaseIDGenerator<T extends CompoundResource> {
+  protected readonly prefix: string = "MDC";
+  protected abstract modelName: string;
+  protected modelInstance: T;
+  protected currentId: string | undefined = undefined;
+  protected error: string = "";
 
-  constructor(name: string, instance: T, idx: string | undefined) {
-    this.modelName = name;
+  constructor(instance: T, idx: string | undefined) {
     this.modelInstance = instance;
     this.currentId = idx;
+    this.setError();
   }
 
-  private generateStaffId = async () => {
+  protected abstract generateId(): Promise<boolean>;
+  protected setError = () =>
+    (this.error = `Failed to generate id for ${this.modelName.toLowerCase()}`);
+
+  public generate = async () => {
+    if (this.currentId) return { status: true, data: this.currentId };
+
+    const status = await this.generateId();
+    if (!status) return { status, data: this.error };
+    return { status, data: this.currentId as string };
+  };
+}
+
+export class StaffIdGenerator extends BaseIDGenerator<CompoundResource> {
+  protected readonly modelName = "Staff";
+
+  constructor(instance: CompoundResource, idx: string) {
+    super(instance, idx);
+    this.setError();
+  }
+
+  protected generateId = async () => {
     try {
       const staffCount = await mongoose
         .model(this.modelName)
         .countDocuments({ chpsCompoundId: this.modelInstance.chpsCompoundId });
       const index = `${staffCount + 1}`.padStart(4, "0");
       this.currentId = `${this.prefix}S${index}`;
+      return true;
     } catch (err) {
       const { message } = err as Error;
       logger.error({ action: "generateStaffId", msg: message });
+      return false;
     }
   };
+}
+export class PatientIdGenerator extends BaseIDGenerator<CompoundResource> {
+  protected readonly modelName = "Patient";
 
-  private generatePatientId = async () => {
+  constructor(instance: CompoundResource, idx: string) {
+    super(instance, idx);
+    this.setError();
+  }
+
+  protected generateId = async () => {
     try {
-      const patientCount = await mongoose
-        .model(this.modelName)
-        .countDocuments({ chpsCompoundId: this.modelInstance.chpsCompoundId });
+      const patientCount = await mongoose.model(this.modelName).countDocuments({
+        chpsCompoundId: this.modelInstance.chpsCompoundId,
+      });
       const chps = await mongoose
         .model("ChpsCompound")
         .findOne({ _id: this.modelInstance.chpsCompoundId });
       const index = `${patientCount + 1}`.padStart(3, "0");
       this.currentId = this.prefix + chps.getInitials() + index;
+      return true;
     } catch (err) {
       const { message } = err as Error;
       logger.error({ action: "generatePatientId", msg: message });
+      return false;
     }
-  };
-
-  private methods: Record<string, () => Promise<void>> = {
-    Staff: this.generateStaffId,
-    Patient: this.generatePatientId,
-  };
-
-  public generate = async () => {
-    if (this.currentId) return { status: true, data: this.currentId };
-    await this.methods[this.modelName]();
-    return {
-      status: !!this.currentId,
-      data: !this.currentId ? this.error : (this.currentId as string),
-    };
   };
 }
