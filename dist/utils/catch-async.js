68da53960e91d30f2bc2ef735e974d2a754cb6f0
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2024-08-03 08:02:29 +0400

    removed

--- dist/utils/catch-async.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsync = void 0;
-function catchAsync(handler) {
-    return (req, res, next) => {
-        handler(req, res, next).catch(next);
-    };
-}
-exports.catchAsync = catchAsync;
